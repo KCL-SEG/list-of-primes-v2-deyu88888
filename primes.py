ad292f6f672744b169807cab5140553e056377c7
Deyu Li <k21081622@kcl.ac.uk>
2023-12-08 12:51:17 +0000

    complete prime

--- primes.py
+++ primes.py
@@ -3,4 +3,22 @@
 
 def primes(number_of_primes):
     list = []
+    num = 2
+
+
+    if number_of_primes <= 0:
+        raise ValueError("number_of_primes cannot be less or equal to 0")
+
+    while len(list)<number_of_primes:
+        is_prime = True
+
+        for j in range(2, num // 2 + 1):
+            if num % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            list.append(num)
+        num += 1
+
+
     return list
